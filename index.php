bc78ef8dc8c830fafbc0f1970d69a8c9fc08e52e
rishabhjain0058 <rishabh.cloud@gmail.com>
2020-04-17 13:11:42 +0530

    final deployment

--- index.php
+++ index.php
@@ -24,7 +24,7 @@ else
 <body>
     <section class="congratulations">
         <h1>Congratulations!</h1>
-		<h3>This is version 2</h3>
+		<h3>This is version 3</h3>
         <p>Your AWS Elastic Beanstalk <em>PHP</em> application is now running on your own dedicated environment in the AWS&nbsp;Cloud</p>
         <p>You are running PHP version <?= phpversion() ?></p>
     </section>
